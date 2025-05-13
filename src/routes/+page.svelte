8dffaec976a143238c8f4d2627b9beaed816e7f4
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-05-13 11:30:22 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Toaster, toast } from 'svelte-sonner';
+</script>
+
+<Toaster richColors closeButton />
+
+<button onclick={() => toast.info('Info')}> Info </button>
+<button onclick={() => toast.success('Success')}> Success </button>
+<button onclick={() => toast.warning('Warning')}> Warning </button>
+<button onclick={() => toast.error('Error')}> Error </button>
